fb272c394877d75ee979afe91d2fea485828360e
Luke Buthman <lbuthman@gmail.com>
2017-11-19 14:16:46 -0600

    init commit

--- ex-in-java/src/lbuthman/exercise48/GrabbingWeather.java
+++ ex-in-java/src/lbuthman/exercise48/GrabbingWeather.java
@@ -1,4 +1,13 @@
 package lbuthman.exercise48;
 
+import java.util.Scanner;
+
+import static tools.Tools.*;
+
 public class GrabbingWeather {
+    public static void main(String[] args) {
+        Scanner input = getScanner();
+
+
+    }
 }
